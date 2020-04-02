49c93f517f707b35d7a3bee37c86a8e286b65c8b
Shin YeongJae <syj9484@gmail.com>
2020-04-02 22:17:56 +0900

    modal pop up practice initial commit

---
+++ modaljs.js
@@ -0,0 +1,7 @@
+document.getElementById("modal-open").onclick = function() {
+    document.getElementById("modal").style.display="block";
+}
+
+document.getElementById("modal-close").onclick = function() {
+    document.getElementById("modal").style.display="none";
+} 
